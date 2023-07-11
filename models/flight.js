0252e85a30ced6ed523c4179e9d5947a2e7619f8
Mabel Lam <mabellam@Mabels-iMac.local>
2023-07-10 18:27:53 -0700

    flights model

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,35 @@ const mongoose = require('mongoose');
 // shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+//define flight schema
 const flightSchema = new Schema({
-    
-})
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United'],
+        required: true
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'LAX', 'SAN'],
+        required: true
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function(){
+            const oneYearFromNow = new Date();
+            oneYearFromNow.setFullYear(oneYearFromNow()+1);
+            return oneYearFromNow;
+        }
+    },
+    destinations: [destinationsSchema]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
